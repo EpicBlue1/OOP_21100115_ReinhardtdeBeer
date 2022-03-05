38b993fafb023decee400e58a2b4284153380c94
EpicBlue1 <21100115@virtualwindow.co.za>
2022-03-05 13:07:53 +0200

    Bootstrap implemented
    Basic Navbar, responsive. Not working yet.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
-import './App.css';
+import './index.css';
+import Header from "./components/Header";
+import {Route, Routes} from 'react-router-dom';
 
 function App() {
   return (
     <>
+      <div className="App">
+
+      <Header />
+
+    </div>
     </>
   );
 }
